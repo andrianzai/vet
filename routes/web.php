2628561d3a77f1c9b08e76468655921a1550943b
andrianzai <andrianrion15@gmail.com>
2024-08-12 21:38:47 +0800

    restart

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,3 @@ use App\Http\Controllers\BarangController;
 Route::get('/', function () {
     return view('content.dashboard');
 });
-
-Route::get('/transaksi', function () {
-    return view('content.transaksi');
-});
-
-Route::get('/barang', [BarangController::class, 'index'])->name('barang.index');
-Route::post('/barang', [BarangController::class, 'store'])->name('barang.store');
-
-Route::put('/barang/{product_code}', [BarangController::class, 'update'])->name('barang.update');
-Route::delete('/barang/{barang}', [BarangController::class, 'destroy'])->name('barang.destroy');
